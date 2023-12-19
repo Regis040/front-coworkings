7ba45d3c8f8c02cf0b0904fa371e8cdcca635979
Regis JOUAN <regis.jouan@lapiscine.pro>
2023-12-19 15:57:27 +0100

    add complete UPDATE with my comments

--- src/page/admin/AdminCoworkingUpdate.jsx
+++ src/page/admin/AdminCoworkingUpdate.jsx
@@ -4,8 +4,10 @@ import { useParams } from "react-router-dom";
 const AdminCoworkingUpdate = () => {
     // fonction hook qui permet de prendre l'id en paramètre et donc d'identifier l'élement concerné
   const { id } = useParams();
-//   fonction hook qui permet da'fficher deux rendus; le premier null et le second avec le coworking identifié
+//   fonction hook qui permet d'afficher deux rendus; le premier null et le second avec le coworking identifié
   const [coworking, setCoworking] = useState(null);
+
+  const [message, setMessage] = useState(null);
 // fonction hook qui permet le stockage dans la variable coworking (via la fonction setCoworking), une fois, des données attendues en async
 // les données sont récupèrées dans le body avec le "si coworking existe alors afficher les données dans le formulaire"
   useEffect(() => {
@@ -16,14 +18,70 @@ const AdminCoworkingUpdate = () => {
     })();
   }, [id]);
 
+// Je récupère les lignes dans AdminCoworkingsPage:
+// qui avec un clic sur le submit récupère toutes les données du formulaire
+  const handleUpdateCoworking = async (event) => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const priceByMonth = event.target.priceByMonth.value;
+    const priceByDay = event.target.priceByDay.value;
+    const priceByHour = event.target.priceByHour.value;
+    const addressNumber = event.target.addressNumber.value;
+    const addressStreet = event.target.addressStreet.value;
+    const addressCity = event.target.addressCity.value;
+    const addressPostcode = event.target.addressPostcode.value;
+    const superficy = event.target.superficy.value;
+    const capacity = event.target.capacity.value;
+
+// ... les données sont placées dans un objet dont chaque nom est l'identique du mofdel de l'API
+    const coworkingUpdateData = {
+      name: name,
+      price: {
+        month: priceByMonth,
+        day: priceByDay,
+        hour: priceByHour,
+      },
+      address: {
+        number: addressNumber,
+        street: addressStreet,
+        city: addressCity,
+        postCode: addressPostcode,
+      },
+      superficy: superficy,
+      capacity: capacity,
+    };
+// Les données sont transformé en json
+    const coworkingUpdateDataJson = JSON.stringify(coworkingUpdateData);
+// On récupère le token dans le localStorage
+    const token = localStorage.getItem("jwt");
+// On effectue un fetch en métode PUT sur une id via l'API
+// autorisée grâce token récupéré
+// Les données de ensuite envoyé sur le body
+    const updateCoworkingResponse = await fetch("http://localhost:3000/api/coworkings/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: coworkingUpdateDataJson,
+    });
+// on gére la mise à jour du coworking identifié par un message
+    if (updateCoworkingResponse.status === 201) {
+      setMessage("Mise à jour OK");
+    } else {
+      setMessage("Erreur");
+    }
+  };
+
   return (
     <div>
     {/* si le coworking existe alors je récupère le formulaire pré-rempli avec les données du coworking identifié avec son id
     le "defaultvalue" permet la modification des champs et donc leur mis à jour 
-    */}
+     + affichage du message qui gère l'échec ou la réussite de la mise à jour*/}
+    <>{message && <p>{message}</p>}</>
       {coworking && (
-        
-        <form>
+        <form onSubmit={handleUpdateCoworking}>
           <div>
             <label>
               Nom
